4d6c787c1ea22c3a334779f81ee58872b860a6c1
vinybergamo <vinybergamo@gmail.com>
2022-09-19 12:05:16 -0300

    Feat: Modal Discord User Implemented

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -2,6 +2,7 @@ import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import { Entypo } from "@expo/vector-icons";
+import { useEffect, useState } from "react";
 
 import logoImg from "../../assets/logo-nlw-esports.png";
 
@@ -12,10 +13,11 @@ import { GameParams } from "../../@types/navigation";
 import { Heading } from "../../components/Heading";
 import { Background } from "../../components/Background";
 import { DuoCard, DuoCardProps } from "../../components/DuoCard";
-import { useEffect, useState } from "react";
+import { DuoMatch } from "../../components/DuoMatch";
 
 export function Game() {
   const [duos, setDuos] = useState<DuoCardProps[]>([]);
+  const [selectedUser, setSelectedUser] = useState("");
 
   const route = useRoute();
   const game = route.params as GameParams;
@@ -26,8 +28,14 @@ export function Game() {
     navigate.goBack();
   }
 
+  const getDiscordUser = (adsId: String) => {
+    fetch(`http://192.168.38.34:3333/ads/${adsId}/discord`)
+      .then((response) => response.json())
+      .then((data) => setSelectedUser(data.discord));
+  };
+
   useEffect(() => {
-    fetch(`http://192.168.239.192:3333/games/${game.id}/ads`)
+    fetch(`http://192.168.38.34:3333/games/${game.id}/ads`)
       .then((response) => response.json())
       .then((data) => setDuos(data));
   }, []);
@@ -66,13 +74,18 @@ export function Game() {
           data={duos}
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
-            <DuoCard data={item} onConnect={() => {}} />
+            <DuoCard data={item} onConnect={() => getDiscordUser(item.id)} />
           )}
           ListEmptyComponent={() => (
             <Text style={styles.emptyListText}>
               Não há anúncios publicados para este game!
             </Text>
           )}
+        />
+        <DuoMatch
+          onClose={() => setSelectedUser("")}
+          visible={selectedUser.length > 0}
+          discord={selectedUser}
         />
       </SafeAreaView>
     </Background>
